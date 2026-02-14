8f8984449cd026dee8a5c1084699aa5588a9a1a8
Ansecor <matthewbalas@hotmail.com>
2026-02-14 17:52:34 -0500

    Update planet.py

--- physics/planet.py
+++ physics/planet.py
@@ -2,7 +2,7 @@ class Planet:
     def __init__(self, mass, radius, distToSun, name, rebound_particle=None):
         self.mass = mass
         self.radius = radius
-        self.disToSun = distToSun
+        self.distToSun = distToSun
         self.name = name
         self.exists = True
         self.rebound_particle = rebound_particle # link to the particle in the simulation
