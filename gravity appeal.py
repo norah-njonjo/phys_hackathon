a205d403510723d18bfb0f0ed4216db6d92ebaa9
Advetaa Khurana <ak9741@Addys-Mac.local>
2026-02-15 10:48:25 -0500

    gmh

--- gravity appeal.py
+++
@@ -1,8 +0,0 @@
-"""
-Applying laws of Gravitational Attraction
-    - the bigger the mass , the bigger Fg
-    - the smaller the mass, the less attractive the force on the player from the planets 
-"""
-#if the masses are equal nothing happens 
-#absorption_margin = 0.1
-
